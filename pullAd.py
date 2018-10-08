08785145166b34cbedda88524b2f4795444eb168
billmenschner <43903040+billmenschner@users.noreply.github.com>
2018-10-08 00:02:14 -0400

    First attempt at finding and saving the text information for each of the major job sites.

---
+++ pullAd.py
@@ -0,0 +1,9 @@
+!# Python3
+
+# pullAd.py -- Asks users to paste address of job ad. Once pasted, determines
+# the site of the ad, determines type of job, and pulls text description of job.
+
+import requests, bs4
+
+
+
