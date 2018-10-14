6f7a447c5830dc587895c65ca7235cff0aa27175
billmenschner <43903040+billmenschner@users.noreply.github.com>
2018-10-14 03:48:15 -0400

    Added remove punctuation, single word list, and double word list.

--- pullAd.py
+++ pullAd.py
@@ -3,7 +3,8 @@
 # pullAd.py -- Asks users to paste address of job ad. Once pasted, determines
 # the site of the ad, determines type of job, and pulls text description of job.
 
-import JobSites
+import pandas as pd, numpy as np, matplotlib.pyplot as plt, seaborn as sns, re
+from JobSites import *
 from collections import Counter
 
 #User enters websites
@@ -24,14 +25,45 @@ elif 'craigslist' in website:
     job_ad = craigslist(website)
 elif 'ziprecruiter' in website:
     job_ad = ziprecruiter(website)
+    
+#Strip punctuation, make everything lowercase, remove new lines.
 
+clean_job_ad = re.sub(r"""[,.;?!'@#$%&*"()/’:-]|\— #Punctuation to remove.
+                           """,
+                           " ", #Replace with a single space.
+                           job_ad, flags = re.VERBOSE).lower().replace('\n', ' ')
 
+#Create single-word list, eliminating articles and other superfluous words.
+
+single_word_list = re.sub(r"""\band\b|\bthe\b|\bof\b|\bis\b|\bto\b|\bin\b|
+    \bfor\b|\ba\b|\bwith\b|\ban\b|\ball\b|\bthat\b|\bas\b|\bit\b|\bits\b|
+    \bon\b|\bat\b""",
+                     " ",
+                     clean_job_ad, flags = re.VERBOSE).split(' ')
+
+#Create double-word and triple-word lists.
+
+job_ad_list = clean_job_ad.split()
+doublet_list = []
+for word in job_ad_list:
+    doublet = word + ' ' + job_ad_list[job_ad_list.index(word) + 1]
+    doublet_list.append(doublet)
+
+print(Counter(doublet_list))
+    
+
+   
 # Very basic word count list. This kind of works, but it would be better to not
 #see words repeated. Also need to get rid of duplicate words. Also need to look
 #into multi-word combos.
 #for word in job_ad.split(' '):
 #    print(word + ' = ' + str(job_ad.count(word)))
 #    
-#word_list = job_ad.split(' ')
+#word_list = clean_job_ad.split(' ')
 #
-#print(Counter(word_list).most_common(5))
+#d = Counter(word_list)
+#
+#df = pd.DataFrame(d, index=[0])
+#df.head(10)
+
+#print(Counter(single_word_list))
